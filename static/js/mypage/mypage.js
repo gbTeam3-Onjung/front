1a2b33ea8809c19ad46b7328dc7648997fd5806f
hmk4079 <hmk4079@gmail.com>
2024-10-14 00:54:54 +0900

    정상부분 pr

--- static/js/mypage/mypage.js
+++ static/js/mypage/mypage.js
@@ -17,42 +17,25 @@ const showTab = (tabId, element) => {
     // 현재 클릭된 요소의 부모 li에 active 클래스 추가
     element.parentElement.classList.add("active");
 };
-document.addEventListener("click", () => {
-    // 데이터 편집 가능 요소 설정
-    const editableElements = document.querySelectorAll(".data-editable");
 
-    editableElements.forEach((element) => {
-        // 각 요소에 대해 반복적으로 처리하는 코드 블록을 정의
-        element.addEventListener("click", () => {
-            const currentText = element.innerText;
-            const input = document.createElement("input");
-            input.type = "text";
-            // 생성된 입력 필드의 유형을 text로 설정
+//마우스 포인트가 올라가있는 태그 화면 css적용
+document.addEventListener("DOMContentLoaded", function () {
+    const screens = document.querySelectorAll(".screen"); // 클래스명이 screen인 태그들을 선택합니다.
 
-            input.value =
-                currentText.trim() === "정보를 입력해주세요."
-                    ? ""
-                    : currentText.trim();
-            // 만약 현재 텍스트가 "정보를 입력해주세요."인 경우, 입력 필드를 비워두고
-            // 그렇지 않으면, 기존 텍스트를 입력 필드에 표시
-            // trim() 메서드는 텍스트의 앞뒤 공백 제거
-            element.innerHTML = "";
-            element.appendChild(input);
-            input.focus();
-            input.addEventListener("blur", () => {
-                const newText =
-                    input.value.trim() === ""
-                        ? "정보를 입력해주세요."
-                        : input.value.trim();
-                // 만약 입력 필드가 비어 있다면, 기본 메시지("정보를 입력해주세요.")를 사용
-                element.innerText = newText;
-            });
-
-            // input.addEventListener("keydown", (e) => {
-            //     if (e.key === "Enter") {
-            //         input.blur();
-            //     }
-            // });
+    screens.forEach((screen) => {
+        screen.addEventListener("mouseenter", () => {
+            applyHoverEffect(screen);
+        });
+        screen.addEventListener("mouseleave", () => {
+            removeHoverEffect(screen);
         });
     });
+
+    function applyHoverEffect(activeScreen) {
+        activeScreen.classList.add("active"); // 화면에 active 클래스를 추가하여 CSS가 적용되도록 함
+    }
+
+    function removeHoverEffect(screen) {
+        screen.classList.remove("active"); // 화면에서 active 클래스를 제거하여 CSS가 원래대로 돌아가도록 함
+    }
 });
