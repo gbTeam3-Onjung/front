e9e8dc363bfd7254d1d4b33a20890edd7aa6f66a
hmk4079 <hmk4079@gmail.com>
2024-10-15 10:48:05 +0900

    1015 10:47작업

--- static/js/mypage/mypage-payment.js
+++ static/js/mypage/mypage-payment.js
@@ -15,6 +15,22 @@ const calendar2 = document.querySelector(
 // date-range-icon 클래스를 가진 svg 요소 가져오기
 const dateRangeIcon = document.querySelector(".cAfwXx.date-range-icon");
 
+// 오늘의 년도와 월을 설정하기 위한 함수
+function setTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // 월은 0부터 시작하므로 +1 필요
+
+    // rdrMonthAndYearPickers 클래스를 가진 요소에 오늘의 년도와 월 설정
+    const monthAndYearPickers = document.querySelectorAll(
+        ".rdrMonthAndYearPickers"
+    );
+    monthAndYearPickers.forEach((picker) => {
+        picker.querySelector(".year-selector").textContent = year;
+        picker.querySelector(".month-selector").textContent = month;
+    });
+}
+
 // date-input 및 date-range-icon 클릭 시 동작 정의
 function handleDateInputClick(event) {
     event.stopPropagation(); // 클릭 이벤트 전파 막기
@@ -27,6 +43,10 @@ function handleDateInputClick(event) {
     if (isHidden) {
         calendar1.style.display = "block"; // 첫 번째 캘린더 표시
         calendar2.style.display = "block"; // 두 번째 캘린더 표시
+        setTodayDate(); // 오늘의 년도와 월 설정
+    } else {
+        calendar1.style.display = "none"; // 첫 번째 캘린더 숨기기
+        calendar2.style.display = "none"; // 두 번째 캘린더 숨기기
     }
 }
 
@@ -37,10 +57,10 @@ dateRangeIcon.addEventListener("click", handleDateInputClick);
 // date-input, dateRangeIcon, calendar1, calendar2 내부의 요소를 제외하고 클릭하면 active 클래스 제거 및 캘린더 숨기기
 document.addEventListener("click", function (event) {
     if (
-        !dateInput.contains(event.target) &&
-        !dateRangeIcon.contains(event.target) &&
-        !calendar1.contains(event.target) &&
-        !calendar2.contains(event.target)
+        !event.target.closest(".date-input") &&
+        !event.target.closest(".cAfwXx.date-range-icon") &&
+        !event.target.closest(".rdrDateRangePickerWrapper.date-range-picker") &&
+        !event.target.closest(".rdrCalendarWrapper.rdrDateRangeWrapper")
     ) {
         dateRange.classList.remove("active");
         calendar1.style.display = "none"; // 첫 번째 캘린더 숨기기
