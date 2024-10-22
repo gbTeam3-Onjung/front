14bed174182ab0e6cbe72bcd411e3c790005991e
jimin9712 <ljm21000@gmail.com>
2024-10-22 17:03:15 +0900

    __ to -

--- static/js/admin/admin_product.js
+++ static/js/admin/admin_product.js
@@ -1,63 +1,63 @@
 // 모든 메뉴 아이템과 서브메뉴 컨테이너를 선택합니다.
 const menuItems = document.querySelectorAll(
-    ".MenuItems_menu__QgJck.MenuItems_hasSubmenu__1EhTS"
+    ".MenuItems_menu-QgJck.MenuItems_hasSubmenu-1EhTS"
 );
 
 menuItems.forEach((menuToggle) => {
     menuToggle.addEventListener("click", function () {
         const submenuContainer = menuToggle.nextElementSibling;
         const downIcon = menuToggle.querySelector(
-            ".withIcon_icon__2IQiS.MenuItems_downIcon__2GVY9"
+            ".withIcon_icon-2IQiS.MenuItems_downIcon-2GVY9"
         );
 
         // 서브메뉴의 표시/숨김을 토글합니다.
-        submenuContainer.classList.toggle("MenuItems_show__1ldUA");
-        downIcon.classList.toggle("MenuItems_open__2VtAS");
+        submenuContainer.classList.toggle("MenuItems_show-1ldUA");
+        downIcon.classList.toggle("MenuItems_open-2VtAS");
     });
 });
 
 // 정보 버튼 클릭 시 정보 박스 표시/숨김
-const infoButton = document.querySelector(".ProjectInfo_infoButton__2bg95");
+const infoButton = document.querySelector(".ProjectInfo_infoButton-2bg95");
 
 infoButton.addEventListener("click", function () {
     const isExpanded = infoButton.getAttribute("aria-expanded") === "true";
     infoButton.setAttribute("aria-expanded", !isExpanded);
 
-    const container = document.querySelector(".ProjectInfo_container__3jAST");
+    const container = document.querySelector(".ProjectInfo_container-3jAST");
     const existingInfoBox = document.querySelector(
-        ".ProjectInfo_infoBox__2x8RL"
+        ".ProjectInfo_infoBox-2x8RL"
     );
 
     if (isExpanded && existingInfoBox) {
         container.removeChild(existingInfoBox);
     } else if (!isExpanded) {
         const infoBox = document.createElement("div");
-        infoBox.className = "ProjectInfo_infoBox__2x8RL";
+        infoBox.className = "ProjectInfo_infoBox-2x8RL";
         infoBox.setAttribute("role", "region");
         infoBox.setAttribute("aria-labelledby", "project-info");
 
         infoBox.innerHTML = `
             <dl>
-                <dt class="BlindText_textHidden__1W0aB">메이커 명</dt>
-                <dd class="ProjectInfo_content__1VmXE">
-                    <span class="Avatar_avatar__1mOG5 Avatar_sm__V_6km ProjectInfo_profileImage__1PLV0">
-                        <span class="Avatar_inner__hN779">
+                <dt class="BlindText_textHidden-1W0aB">메이커 명</dt>
+                <dd class="ProjectInfo_content-1VmXE">
+                    <span class="Avatar_avatar-1mOG5 Avatar_sm-V_6km ProjectInfo_profileImage-1PLV0">
+                        <span class="Avatar_inner-hN779">
                             <img src="https://static.wadiz.kr/studio/funding/static/media/default-zingugi.de76a099.svg" />
                         </span>
                     </span>
-                    <span class="ProjectInfo_makerName__KIDrd">석상훈</span>
+                    <span class="ProjectInfo_makerName-KIDrd">석상훈</span>
                 </dd>
-                <dt class="BlindText_textHidden__1W0aB">상태</dt>
-                <dd class="ProjectInfo_content__1VmXE ProjectInfo_stateBox__705_4">
-                    <span class="ProjectInfo_state__1Aqkt">작성 중</span>
+                <dt class="BlindText_textHidden-1W0aB">상태</dt>
+                <dd class="ProjectInfo_content-1VmXE ProjectInfo_stateBox-705_4">
+                    <span class="ProjectInfo_state-1Aqkt">작성 중</span>
                 </dd>
             </dl>
-            <button class="Button_button__35X6_ Button_primary__1vdas Button_contained__1XlJQ Button_sm__3vobZ Button_startIcon__7ZEht Button_block__3y0pW" type="button">
+            <button class="Button_button-35X6_ Button_primary-1vdas Button_contained-1XlJQ Button_sm-3vobZ Button_startIcon-7ZEht Button_block-3y0pW" type="button">
                 <span>
-                    <svg viewBox="0 0 40 40" focusable="false" role="presentation" class="withIcon_icon__2IQiS Button_icon__2I54k" aria-hidden="true">
+                    <svg viewBox="0 0 40 40" focusable="false" role="presentation" class="withIcon_icon-2IQiS Button_icon-2I54k" aria-hidden="true">
                         <path d="M33.6 5.2a9 9 0 0 1 0 12.7L29 22.5l-.6.5a11 11 0 0 0-.4-2.4l4.1-4.1a7 7 0 0 0-9.9-9.9l-4.6 4.6a7 7 0 0 0 4.7 11.9 5 5 0 0 1-.2 2 8.9 8.9 0 0 1-7.8-5.4 9.1 9.1 0 0 1-.3-6.5 8.9 8.9 0 0 1 2.1-3.4l4.6-4.6a9 9 0 0 1 12.9 0zm-15.7 9.5a5 5 0 0 0-.2 2 6.9 6.9 0 0 1 6.3 4.2 7 7 0 0 1-1.5 7.7l-5.7 5.7a7 7 0 0 1-9.9-9.9l5.2-5.2a11 11 0 0 1-.4-2.4l-.6.5-5.6 5.6a9 9 0 1 0 12.7 12.8l5.7-5.7a8.9 8.9 0 0 0 2.1-3.4 9.1 9.1 0 0 0-.3-6.5 8.9 8.9 0 0 0-7.8-5.4z"></path>
                     </svg>
-                    <span class="Button_children__1TD4r">URL 복사</span>
+                    <span class="Button_children-1TD4r">URL 복사</span>
                 </span>
             </button>
         `;
@@ -68,24 +68,24 @@ infoButton.addEventListener("click", function () {
 // 사이드바 사라짐
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.querySelector(
-        "button.AppLayout_expandNavButton__2AQMd"
+        "button.AppLayout_expandNavButton-2AQMd"
     );
     const nav = document.getElementById("AppNavbarLayout_Nav");
-    const mainDiv = document.querySelector("div.AppLayout_main__3h4EB");
+    const mainDiv = document.querySelector("div.AppLayout_main-3h4EB");
     let dividerAdded = false;
 
     button.addEventListener("click", function () {
-        button.classList.toggle("AppLayout_expand__3TNUI");
-        nav.classList.toggle("AppNavbarLayout_expand__12bTj");
+        button.classList.toggle("AppLayout_expand-3TNUI");
+        nav.classList.toggle("AppNavbarLayout_expand-12bTj");
 
         if (!dividerAdded) {
             const newDivider = document.createElement("div");
-            newDivider.className = "AppNavbarLayout_divider__KAkAr";
+            newDivider.className = "AppNavbarLayout_divider-KAkAr";
             mainDiv.insertBefore(newDivider, nav);
             dividerAdded = true;
         } else {
             const existingDivider = mainDiv.querySelector(
-                ".AppNavbarLayout_divider__KAkAr"
+                ".AppNavbarLayout_divider-KAkAr"
             );
             if (existingDivider) {
                 mainDiv.removeChild(existingDivider);
@@ -130,14 +130,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const saveBtn = document.querySelector(".verify-btn.save"); //수정하기 저장 버튼
     const sortFilterOptions = document.querySelectorAll(".sort-filter-option"); //결제일 순/ 결제 수단 선택
     const viewBtn = document.querySelector(".viewDetail"); // 상세보기 버튼
-    const viewDetail = document.querySelectorAll(".UserTable_row__1Qg9b"); //데이터 행을 누르면
+    const viewDetail = document.querySelectorAll(".UserTable_row-1Qg9b"); //데이터 행을 누르면
 
     let actionToPerform = null; // 전역 변수로 초기화
 
     // 활동중, 탈퇴함 글자 색
-    document.querySelectorAll(".UserTable_row__1Qg9b").forEach((row) => {
+    document.querySelectorAll(".UserTable_row-1Qg9b").forEach((row) => {
         const statusCell = row.querySelector(
-            ".UserTable_cell__3kj0K:nth-child(8)"
+            ".UserTable_cell-3kj0K:nth-child(8)"
         );
         if (statusCell) {
             const statusText = statusCell.textContent.trim();
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     // 체크박스 클릭 시 이벤트 전파 방지
     const checkboxes = document.querySelectorAll(
-        ".UserTable_cell__3kj0K:nth-child(1)"
+        ".UserTable_cell-3kj0K:nth-child(1)"
     );
     checkboxes.forEach((checkbox) => {
         checkbox.addEventListener("click", (event) => {
@@ -235,7 +235,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //             ".userCheckbox:checked"
     //         );
     //         selectedCheckboxes.forEach((checkbox) => {
-    //             const row = checkbox.closest(".UserTable_row__1Qg9b");
+    //             const row = checkbox.closest(".UserTable_row-1Qg9b");
     //             row.parentNode.removeChild(row);
     //         });
     //     };
@@ -256,11 +256,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     // 행 선택시 상세보기
     // 테이블의 모든 행을 선택
-    document.querySelectorAll(".UserTable_row__1Qg9b").forEach((row) => {
+    document.querySelectorAll(".UserTable_row-1Qg9b").forEach((row) => {
         // 각 행에 클릭 이벤트 리스너 추가
         row.addEventListener("click", () => {
             // 클릭한 대상이 헤더가 아니라면 모달을 엽니다.
-            if (!row.classList.contains("UserTable_header__2WvXj")) {
+            if (!row.classList.contains("UserTable_header-2WvXj")) {
                 openModal("edit");
             }
         });
